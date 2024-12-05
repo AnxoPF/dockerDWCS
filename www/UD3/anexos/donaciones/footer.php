69db6cb80cb248aea644f745bf5396088134d340
AnxoPF <a23anxopf@iessanclemente.net>
2024-12-05 14:30:04 +0100

    arreglo

--- www/UD3/anexos/donaciones/footer.php
+++ www/UD3/anexos/donaciones/footer.php
@@ -1,3 +1,3 @@
 <footer class="bg-secondary text-dark text-center text-white py-2">
-    <p>© 2024 Marco Magán Sanz - Todos los derechos reservados.</p>
+    <p>© 2024 Anxo Pacheco Fuentes - Todos los derechos reservados.</p>
 </footer>
