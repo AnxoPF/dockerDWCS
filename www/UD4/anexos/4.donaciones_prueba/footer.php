36eac50989f0830ab68cf0fda5161071fa27792d
AnxoPF <a23anxopf@iessanclemente.net>
2025-03-07 18:12:36 +0100

    inicio ud4 ej4

---
+++ www/UD4/anexos/4.donaciones_prueba/footer.php
@@ -0,0 +1,3 @@
+<footer class="bg-secondary text-dark text-center text-white py-2">
+    <p>© 2024 Anxo Pacheco Fuentes - Todos los derechos reservados.</p>
+</footer>
---
+++ www/UD4/anexos/4.donaciones_prueba/header.php
@@ -0,0 +1,4 @@
+<header class="bg-success text-white text-center py-3">
+    <h1>Donaciones</h1>
+    <p>Solución unidad 4 anexo 4</p>
+</header>
